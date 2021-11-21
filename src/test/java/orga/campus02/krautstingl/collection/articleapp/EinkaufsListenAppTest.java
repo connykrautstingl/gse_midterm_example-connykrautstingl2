c99111d0c3217f604c142220c1976c77b78f47be
connykrautstingl <s44784@edu.campus02.at>
2021-11-21 18:07:31 +0100

    added tests

---
+++ src/test/java/orga/campus02/krautstingl/collection/articleapp/EinkaufsListenAppTest.java
@@ -0,0 +1,17 @@
+package orga.campus02.krautstingl.collection.articleapp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EinkaufsListenAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/orga/campus02/krautstingl/oop/DemoAppTest.java
@@ -0,0 +1,17 @@
+package orga.campus02.krautstingl.oop;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
