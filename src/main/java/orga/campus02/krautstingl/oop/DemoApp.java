b23b703e2a8bbcff25427af55f203256e1263c8a
connykrautstingl <s44784@edu.campus02.at>
2021-11-21 17:41:10 +0100

    add maven

---
+++ src/main/java/orga/campus02/krautstingl/oop/DemoApp.java
@@ -0,0 +1,11 @@
+package orga.campus02.krautstingl.oop;
+
+public class DemoApp {
+    public static void main(String[] args) {
+        Person p = new Person("Max", "Mustermann",
+                183, (byte) 3);
+        p.printPerson(71);
+
+
+    }
+}
