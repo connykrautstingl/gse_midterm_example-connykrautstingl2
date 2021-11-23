0a6b1ee792d3f017bdd15eee07e279c7c7816e5e
connykrautstingl <s44784@edu.campus02.at>
2021-11-23 19:27:54 +0100

    added tests

--- src/test/java/orga/campus02/krautstingl/collection/articleapp/EinkaufsListenAppTest.java
+++ src/test/java/orga/campus02/krautstingl/collection/articleapp/EinkaufsListenAppTest.java
@@ -1,7 +1,7 @@
 package orga.campus02.krautstingl.collection.articleapp;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +9,20 @@ class EinkaufsListenAppTest {
 
     @BeforeEach
     void setUp() {
+
+        double a = 1.79;
+        String s = "Milch";
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void assertTrue(){
+        String expected = "Milch";
+        String lowerCase = this.s.toLowerCase();
+        assertTrue(expected.equals(lowerCase));
+        assertTrue(a > 0);
     }
+    }
+
+
+
 }
---
+++ src/test/java/orga/campus02/krautstingl/oop/PersonTest.java
@@ -0,0 +1,16 @@
+package orga.campus02.krautstingl.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void rechneBMI() {
+    }
+
+    @Test
+    void printPerson() {
+    }
+}
